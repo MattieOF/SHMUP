dbb32b0268d44340de94fa38a9bce9487a725091
Matt Ware <mattbware3@gmail.com>
2023-10-27 19:01:18 +0100

    Basic player movement

---
+++ Scripts/Game/Data/CharacterData.cs
@@ -0,0 +1,9 @@
+using Godot;
+
+[GlobalClass]
+public partial class CharacterData : Resource
+{
+    [Export] public string Name = "Unnamed Character";
+    [Export] public SpriteFrames Sprite;
+    [Export] public float MoveSpeed = 10;
+}
--- Scripts/Game/Player.cs
+++ Scripts/Game/Player.cs
@@ -1,15 +1,34 @@
 using Godot;
-using System;
 
 public partial class Player : AnimatedSprite2D
 {
-	// Called when the node enters the scene tree for the first time.
+	[Export] public CharacterData Data;
+	
 	public override void _Ready()
 	{
+		SpriteFrames = Data.Sprite;
+		Animation = "down";
+		Stop();
 	}
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
+		// Get input
+		int horizontal = (Input.IsActionPressed("right") ? 1 : 0) - (Input.IsActionPressed("left") ? 1 : 0);
+		int vertical = (Input.IsActionPressed("down") ? 1 : 0) - (Input.IsActionPressed("up") ? 1 : 0);
+
+		Vector2 movement = new Vector2(horizontal, vertical);
+		movement = movement.Normalized();
+		movement *= Data.MoveSpeed * (float)delta;
+		
+		Translate(movement);
+		
+		// Select animation
+		if (movement.LengthSquared() == 0)
+			Stop();
+		else if (Mathf.Abs(movement.X) > Mathf.Abs(movement.Y))
+			Play(movement.X < 0 ? "left" : "right");
+		else
+			Play(movement.Y < 0 ? "up" : "down");
 	}
 }

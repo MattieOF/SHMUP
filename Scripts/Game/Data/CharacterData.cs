1bf6177d1f8a13482a3ec0d16fff42ddb9917329
Matt Ware <mattbware3@gmail.com>
2023-11-01 23:41:20 +0000

    Player HP

--- Scripts/Game/Data/CharacterData.cs
+++ Scripts/Game/Data/CharacterData.cs
@@ -6,4 +6,5 @@ public partial class CharacterData : Resource
     [Export] public string Name = "Unnamed Character";
     [Export] public SpriteFrames Sprite;
     [Export] public float MoveSpeed = 300;
+    [Export] public float BaseHealth = 100;
 }

2b1ef357cd30ed46ca69eda0f7d336350dc08335
Matt Ware <mattbware3@gmail.com>
2023-11-02 00:09:42 +0000

    Attempt weighted array type

---
+++ Scripts/Utility/WeightedArray.cs
@@ -0,0 +1,14 @@
+﻿using Godot;
+
+[GlobalClass]
+public partial class WeightedArray<[MustBeVariant] T> : Resource
+{
+    [Export] public Godot.Collections.Dictionary<T, float> Items;
+    public float Total { get; private set; }
+}
+
+[GlobalClass]
+public partial class WeightedSceneArray : WeightedArray<PackedScene>
+{ }
+
+// [Export] public Godot.Collections.Dictionary<T, float> Items;

bae436f748f63cea9a378fef1b2ab85e12cff1ee
Matt Ware <mattbware3@gmail.com>
2023-11-03 00:59:44 +0000

    Bug loot tables

--- Scripts/Game/Data/EnemyData.cs
+++ Scripts/Game/Data/EnemyData.cs
@@ -7,8 +7,8 @@ public partial class EnemyData : Resource
     [Export] public string Name = "Enemy";
     [Export] public float MaxHP = 100;
     [Export] public SpriteFrames Sprite;
-    [Export] public Dictionary<PackedScene, float> GemDrop;
-    [Export] public Dictionary<PackedScene, float> ItemDrop;
+    [Export] public LootTable GemDrop;
+    [Export] public LootTable ItemDrop;
     [Export] public Script EnemyClassOverride;
     [Export] public PackedScene Scene = GD.Load<PackedScene>("res://Scenes/Game/Enemy.tscn");
 }
